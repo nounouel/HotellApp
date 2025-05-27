3903a4ba5bee5eef424b2c2718e67b422633d2da
nounouel <pervietclock@gmail.com>
2025-05-27 14:24:11 +0700

    hotelapp

---
+++ app/src/main/java/com/n222102520/hotelapp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.n222102520.hotelapp;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView _imageView1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        _imageView1 = findViewById(R.id.imageView1 );
+        String imageUrl = "https://alexios.com.gr/wp-content/uploads/2023/10/dining-in-oia-1536x1022.jpg.webp";
+        Picasso.with(this).load(imageUrl).into(_imageView1);
+
+    }
+}
